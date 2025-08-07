c7dcdbe01bcb6ad23bd15c1c5d3cccc4336904c6
stardomains3 <email.box@posteo.net>
2025-08-06 22:47:27 -0400

    Shows citations list if model responds with them(Perplexity)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "io.github.stardomains3.oxproxion"
         minSdk = 34
         targetSdk = 36
-        versionCode = 10
-        versionName = "1.0.9"
+        versionCode = 11
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
