040c411c616976ef9ace2a3795c0f84d0b17255a
stardomains3 <email.box@posteo.net>
2025-11-03 21:27:49 -0500

    Prepare for release v1.7.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "io.github.stardomains3.oxproxion"
         minSdk = 31
         targetSdk = 36
-        versionCode = 72
-        versionName = "1.7.2"
+        versionCode = 73
+        versionName = "1.7.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
